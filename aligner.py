f99f33550ba7d212ef4abf8718816ba327443162
Moritz Hauschulz <meh23@cloud-vm-47-198.doc.ic.ac.uk>
2024-03-10 00:32:35 +0000

    added new scheduler in moritz3.ipynb

---
+++ aligner.py
@@ -0,0 +1,47 @@
+import torch
+from torch.nn import Sequential, Linear, ReLU, Sigmoid, Tanh, Dropout, Upsample
+import torch.nn.functional as F
+import torch.nn as nn
+from torch_geometric.nn import NNConv
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import BatchNorm
+import numpy as np
+from torch_geometric.data import Data
+from torch.autograd import Variable
+from config import N_TARGET_NODES_F, N_SOURCE_NODES_F,N_TARGET_NODES,N_SOURCE_NODES
+
+class Aligner(torch.nn.Module):
+    def __init__(self):
+        
+        super(Aligner, self).__init__()
+
+        nn = Sequential(Linear(1, N_SOURCE_NODES*N_SOURCE_NODES), ReLU())
+        self.conv1 = NNConv(N_SOURCE_NODES, N_SOURCE_NODES, nn, aggr='mean', root_weight=True, bias=True)
+        self.conv11 = BatchNorm(N_SOURCE_NODES, eps=1e-03, momentum=0.1, affine=True, track_running_stats=True)
+
+        nn = Sequential(Linear(1, N_SOURCE_NODES), ReLU())
+        self.conv2 = NNConv(N_SOURCE_NODES, 1, nn, aggr='mean', root_weight=True, bias=True)
+        self.conv22 = BatchNorm(1, eps=1e-03, momentum=0.1, affine=True, track_running_stats=True)
+
+        nn = Sequential(Linear(1, N_SOURCE_NODES), ReLU())
+        self.conv3 = NNConv(1, N_SOURCE_NODES, nn, aggr='mean', root_weight=True, bias=True)
+        self.conv33 = BatchNorm(N_SOURCE_NODES, eps=1e-03, momentum=0.1, affine=True, track_running_stats=True)
+
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.pos_edge_index, data.edge_attr
+
+        x1 = F.sigmoid(self.conv11(self.conv1(x, edge_index, edge_attr)))
+        x1 = F.dropout(x1, training=self.training)
+
+        x2 = F.sigmoid(self.conv22(self.conv2(x1, edge_index, edge_attr)))
+        x2 = F.dropout(x2, training=self.training)
+
+        x3 = torch.cat([F.sigmoid(self.conv33(self.conv3(x2, edge_index, edge_attr))), x1], dim=1)
+        x4 = x3[:, 0:N_SOURCE_NODES]
+        x5 = x3[:, N_SOURCE_NODES:2*N_SOURCE_NODES]
+
+        x6 = (x4 + x5) / 2
+        return x6
+
+
